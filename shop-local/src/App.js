7a067263af1e8160e92a1017c53c31d92b377cbe
Carlos Ortega <caortega4@miners.utep.edu.com>
2021-03-25 19:50:56 -0600

    Create the first version a store's page

--- shop-local/src/App.js
+++ shop-local/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React, { useState } from "react";
+import Item from './components/Item'
+import Header from './components/Header'
+import StoreInfo from './components/Store-info'
 function App() {
+
+  const [items] = useState([
+    { name: "Shampoo", price: "7$" },
+    { name: "Chocolate", price: "1$" },
+    { name: "Milk", price: "4$" },
+    { name: "Bitcoin", price: "999$" },
+    { name: "Candy", price: "0.5$" },
+
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <Header />
+      <StoreInfo name={"Dona antonia"}
+        address={"Pueblito Don Cuco (Paseo de la victoria #2118 5B)"}
+        description={"Un pedacito de Brasil en Cd. Juarez 🇧🇷"}
+        openTime={"8:00 AM"}
+        closeTime={"6:00 PM"}
+        phoneNumber={"526567571621"}
+        instagram={"https://www.instagram.com/donaantoniamx/"}
+        facebook={"https://www.facebook.com/DONAANTONIAMX"}
+      />
+      {//This is what creates an item component for each of the elements in the object 'items'}
+        <div className="itemDisplay">
+          {items.map(item => (
+            <Item name={item.name} price={item.price} />
+          ))}
+        </div>
+
+
+
     </div>
   );
 }
