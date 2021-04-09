c0e11157ed0fb15c8f16ddb0a511ae4606a954f3
Carlos Ortega <caortega4@miners.utep.edu.com>
2021-04-08 20:38:38 -0600

    Fix how the server handles responses

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const mysql = require('mysql'); 
+const mysql = require('mysql');
 
 const PORT = process.env.PORT || 3001;
 
@@ -17,15 +17,15 @@ let connection = mysql.createConnection({
 });
 
 //Create connection
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
-      console.error('Error connecting: ' + err.stack);
-      return;
+        console.error('Error connecting: ' + err.stack);
+        return;
     }
     console.log('Connected as thread id: ' + connection.threadId);
     connection.query("CREATE DATABASE IF NOT EXISTS shoplocal", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
+        if (err) throw err;
+        console.log("Database created");
     });
 });
 
@@ -33,47 +33,105 @@ connection.connect(function(err) {
 function insertStore(storeName, storeAddress, description, openTime, closeTime,
     phoneNumber, instagramLink, facebookLink) {
     var sql = "CREATE TABLE IF NOT EXISTS stores (storeId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255), description VARCHAR(255), openTime VARCHAR(255), closeTime VARCHAR(255), phone VARCHAR(255), instagram VARCHAR(255), facebook VARCHAR(255), item1 VARCHAR(255),  item2 VARCHAR(255), item3 VARCHAR(255), item4 VARCHAR(255))";
-    connection.query(sql, function(err, result) {
+    connection.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Table Created");
     });
 
-    sql = "INSERT INTO stores (name, address, description, openTime, closeTime, phone, instagram, facebook) VALUES ('" + storeName + "', '" + storeAddress + "', '" 
+    sql = "INSERT INTO stores (name, address, description, openTime, closeTime, phone, instagram, facebook) VALUES ('" + storeName + "', '" + storeAddress + "', '"
         + description + "', '" + openTime + "', '" + closeTime + "', '" + phoneNumber + "', '"
         + instagramLink + "', '" + facebookLink + "')";
-    connection.query(sql, function(err, result){
+    connection.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Store inserted");
     });
 }
 
 /* Function to get store information as JSON object */
-function getStore(storeName) {
+async function getStore(storeName) {
     var sql = "SELECT * FROM stores WHERE name = ?";
-    var query = connection.query(sql, [storeName], function(err, result) {
-            if (err) throw err;
-            setResponse(result[0]);
-        });
+    // var query = connection.query(sql, [storeName], function (err, result) {
+    //     if (err) throw err;
+    //     setResponse(result[0]);
+    // });
+    let res = await connection.query(sql, [storeName]);
+    setResponse(res[0]);
 }
-/** Helper function for getStore(storeName)
+
+/* Function to get all store information as an array of JSON objects */
+function getAllStores() {
+    var sql = "SELECT * FROM stores";
+    // var query = connection.query(sql, function (err, result) {
+    //     if (err) throw err;
+    //     setResponse(result);
+    // });
+
+    let res = connection.query(sql);
+    return res;
+}
+
+/* Function to get store information by ID as JSON object */
+async function getStoreByID(storeId) {
+    var sql = "SELECT * FROM stores WHERE storeId = ?";
+    // var query = connection.query(sql, [storeId], function (err, result) {
+    //     if (err) throw err;
+    //     //Only returns first result
+    //     setResponse(result[0]);
+    // });
+    let res = await connection.query(sql, [storeId]);
+    setResponse(res[0]);
+}
+
+/** Helper function for SQL query functions
  * @param {JSON} response - JSON object from getStore query response
  */
 function setResponse(response) {
+
     jsonResponse = response;
 }
 
 /**
- * Handler for store information requests
+ * Handlers for store information requests
  * @param req - expectation of a store name to be passed as storeName
  */
 app.get("/get-store", (req, res) => {
-    getStore(req.query.storeName);
-    res.json(jsonResponse);
-  });
+    var sql = "SELECT * FROM stores WHERE name = ?";
+    var query = connection.query(sql, [storeName], function (err, result) {
+        if (err) throw err;
+        res.json(result[0]);
+    });
+
+});
+
+/** Route to list all stores */
+app.get("/list-stores", (req, res) => {
+
+    var sql = "SELECT * FROM stores";
+    var query = connection.query(sql, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+    });
+
+    //console.log(jsonResponse)
+    //res.json(jsonResponse);
+});
+
+/** Route to get store by ID
+ *  @param storeId - expects an integer value to be passed into query
+ */
+app.get("/get-store-id", (req, res) => {
+    var sql = "SELECT * FROM stores WHERE storeId = ?";
+    var query = connection.query(sql, [req.query.storeId], function (err, result) {
+        if (err) throw err;
+        //Only returns first result
+        res.json(result[0]);
+    });
+
+});
 
 /**
  * Creates NodeJS server on port 3001
  */
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
